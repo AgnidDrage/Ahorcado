09f7868eb234e3f70cecc33fcbc951a72b60a1bb
Mariano Sanchez Toledo <marianosancheztoledo@gmail.com>
2020-10-22 16:24:09 -0300

    AddedRepositoryDificultadUno

--- repository.py
+++ repository.py
@@ -1,2 +1,4 @@
 class Repository():
-    pass
+    dificultadUno={1:{'_palabra':'AUTO', '_tipo_palabra':'VEHICULO'}, 2:{'_palabra':'AVION', '_tipo_palabra':'VEHICULO'},
+                    3:{'_palabra':'HIJO', '_tipo_palabra':'FAMILIAR'}, 4:{'_palabra':'BUSO', '_tipo_palabra':'PROFESION'},
+                    5:{'_palabra':'BYTE','_tipo_palabra':'INFORMATICA'}, 6:{'_palabra':'BIT', '_tipo_palabra':'INFORMATICA'}}
